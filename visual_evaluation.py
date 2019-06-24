41b47cfde7cff2f5413cdb86d9b0f7c8ac13071f
ZZ17390 <kaeru_yuya@hotmail.com>
2019-06-24 18:36:08 +0900

    First Commit

--- visual_evaluation.py
+++ visual_evaluation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from lifelines import KaplanMeierFitter
+from sksurv.nonparametric import kaplan_meier_estimator
 
-path = 'csv/PC200_評価結果.csv'
+path = 'csv/PC200_evalresult.csv'
 
 data = pd.read_csv(path, index_col='SID', delimiter=',')
 
@@ -13,3 +13,9 @@ for i in range(3):
         key = str(i+1)+str(j+1)
         c[key] = data[(data.メンテナンスCID == i+1) & (data.使われ方CID == j+1)]
 
+        figure = plt.figure()
+
+        time, survival_prob = kaplan_meier_estimator(list(map(lambda i : i==1, c[key]["FSTAT"])), c[key]["SMR"])
+        plt.step(time, survival_prob, where="post")
+
+        plt.savefig(key+'.png')
