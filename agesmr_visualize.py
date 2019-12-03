49830fa4482d170087e1253cfc2950beea474836
ZZ17390 <kaeru_yuya@hotmail.com>
2019-12-03 18:25:14 +0900

    Server Commit

--- agesmr_visualize.py
+++ agesmr_visualize.py
@@ -5,6 +5,7 @@ import sys
 import codecs
 import os
 import linecache
+import pylab
 
 path = 'out'
 
@@ -19,9 +20,55 @@ if len(args) > 1:
 '''
 root = [f for f in os.listdir(path) if '1,0_FR.csv' in f]
 
-def graph(data, setting_info, analize_info):
+def scale_image(label, max):
+    fig, ax = plt.subplots(figsize=(10, 10))  # 画像サイズ
+    fig.set_figheight(1)  # 高さ調整
+
+    plt.xlim(0, max)
+    plt.xlabel(label, fontsize=12)
+
+    ax.set_frame_on(False)
+    ax.get_xaxis().tick_bottom()
+    ax.axes.get_yaxis().set_visible(False)
+
+    xmin, xmax = ax.get_xaxis().get_view_interval()
+    ymin, ymax = ax.get_yaxis().get_view_interval()
+    ax.add_artist(pylab.Line2D((xmin, xmax), (ymin, ymin), color='black', linewidth=2))
+
+
+    ax.text(max, -0.05, round(max,2), fontsize=10)
+
+    plt.tight_layout()
+    plt.show()
+
+    plt.close()
+
+
+def data_max(root):
+    n = 0
+    r = 0.0
+
+    for f in root:
+        file = path+'/'+f
+        lines = codecs.open(file, encoding='SJIS').readlines()
+        nmax = int(lines[3].split(',')[1])
+        rmax = float(lines[len(lines)-1].split(',')[3])
+
+        if n < nmax:
+            n = nmax
+
+        if r < rmax:
+            r = rmax
+
+    n += int(n * 0.05)
+    r += r * 0.05
+
+    return n, r
+
+
+def graph(data, setting_info, analize_info, pmax, rmax, xmax):
     fig = plt.figure()
-    fig.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.1, hspace=0.1)
+    fig.subplots_adjust(left=0.05, bottom=0.1, right=0.95, top=0.95, wspace=0.1, hspace=0.1)
     #fig.subplots_adjust(right=0.85)
     #fig.subplots_adjust(left=0.15)
 
@@ -29,22 +76,29 @@ def graph(data, setting_info, analize_info):
     ax2 = ax1.twinx()
     #ax3 = ax1.twinx()
 
-    ax1.set_xlabel(setting_info.split(',')[1] + '(dx=' + setting_info.split(',')[3] + ')')
+    ax1.set_xlabel(setting_info.split(',')[1] + '(dx' + setting_info.split(',')[3] + ')', fontsize=12)
 
-    ax1.set_ylabel('P')
-    ax2.set_ylabel('Rate')
+    ax1.set_ylabel('P', fontsize=12)
+    ax2.set_ylabel('Rate', fontsize=12)
     #ax3.set_ylabel('Count')
 
     #rspine = ax3.spines['right']
     #rspine.set_position(('axes', 1.1))
 
     plt.text(0.01, 0.9, analize_info.replace(',', '\n'), horizontalalignment='left', verticalalignment='top', family='monospace',
-             transform=ax1.transAxes, fontsize=18)
-    plt.xlim(0, 10000)
+             transform=ax1.transAxes, fontsize=12)
+    plt.xlim(0, xmax)
+    ax1.set_ylim(0, pmax)
+    ax2.set_ylim(0, rmax)
 
     ax1.plot(data.index, data['COUNT'], label='Population')
     ax2.plot(data.index, data['RATE'], c='red', label='Failure Rate')
     #ax3.bar(data.index, data['FAIL'], label='Failure Count')
+
+    #軸の数値を削除
+    ax1.set_xticklabels([])
+    ax1.set_yticklabels([])
+    ax2.set_yticklabels([])
 
     #h1, l1 = ax1.get_legend_handles_labels()
     #h2, l2 = ax2.get_legend_handles_labels()
@@ -52,8 +106,8 @@ def graph(data, setting_info, analize_info):
     #ax1.legend(h1 + h2 + h3, l1 + l2 + l3)
     #ax1.legend(h1 + h2, l1 + l2)
 
-    plt.xticks(color="None")
-    plt.yticks(color="None")
+    #plt.xticks(color="None")
+    #plt.yticks(color="None")
 
     plt.show()
 
@@ -61,16 +115,31 @@ def graph(data, setting_info, analize_info):
 
     plt.close()
 
-for f in root:
-    file = path+'/'+f
-    data = pd.read_csv(file, index_col=0, delimiter=',', header = 2, encoding='SJIS')
+def process_file(pmax, rmax, xmax):
+    for f in root:
+        file = path+'/'+f
+        data = pd.read_csv(file, index_col=0, delimiter=',', header = 2, encoding='SJIS')
 
-    lines = codecs.open(file, encoding='SJIS').readlines()
-    setting_info = lines[0].strip()
-    analize_info = lines[1].strip()
+        lines = codecs.open(file, encoding='SJIS').readlines()
+        setting_info = lines[0].strip()
+        analize_info = lines[1].strip()
 
-    print(setting_info)
-    print(analize_info)
-    print(data)
+        print(setting_info)
+        print(analize_info)
+        print(data)
 
-    graph(data, setting_info, analize_info)
+        graph(data, setting_info, analize_info, pmax, rmax, xmax)
+
+if __name__ == '__main__':
+    n, r = data_max(root)
+    print('max N='+str(n)+' max R='+str(r))
+
+    x = 10000
+
+    scale_image('SMR', x)
+    scale_image('Population', n)
+    scale_image('Failure Rate', r)
+
+    exit(0)
+
+    process_file(n, r, x)
